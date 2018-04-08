158e46453f4db728d321e33cff31c864be2b459e
wvanheemstra <willem@vanheemstrasystems.com>
2018-04-08 18:35:41 +0200

    Update feature

--- src/Components/Container/Component.purs
+++ src/Components/Container/Component.purs
@@ -78,13 +78,34 @@ ui =
           ]
         , HH.div
           [ HP.class_ (H.ClassName "col span-1-of-4 box")]
-          [ ]
+          [ HH.i 
+            [ HP.class_ (H.ClassName "ion-ios-stopwatch-outline icon-big")]
+            []
+          , HH.h3_
+            [ HH.text "Ready in 20 minutes"]
+          , HH.p_
+            [ HH.text "You're only twenty minutes away from your delicious and super healthy meals delivered right to your home. We work with the best chefs in each town to ensure that you're 100% happy."]
+          ]
         , HH.div
           [ HP.class_ (H.ClassName "col span-1-of-4 box")]
-          [ ]
+          [ HH.i 
+            [ HP.class_ (H.ClassName "ion-ios-nutrition-outline icon-big")]
+            []
+          , HH.h3_
+            [ HH.text "100% organic"]
+          , HH.p_
+            [ HH.text "All our vegetables are fresh, organic and local. Animals are raised without added hormones or antibiotics. Good for your health, the environment, and it also tastes better!"]
+          ]
         , HH.div
           [ HP.class_ (H.ClassName "col span-1-of-4 box")]
-          [ ]   
+          [ HH.i 
+            [ HP.class_ (H.ClassName "ion-ios-cart-outline icon-big")]
+            []
+          , HH.h3_
+            [ HH.text "Order anything"]
+          , HH.p_
+            [ HH.text "We don't limit your creativity, which means you can order whatever you feel like. You can also choose from our menu containing over 100 delicious meals. It's up to you!"]
+          ]
         ]
       ]
 
